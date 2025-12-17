46089f60cea9c31fcd6beb09626e8ad5fe0b6d65
Yashas-webdev <yashasaryashu@gmail.com>
2025-12-18 00:49:20 +0530

    add the addtocart functionality

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,20 @@ import { ShoppingCart, Heart, Search, Star } from 'lucide-react';
 
 function App() {
   
-  return (
-    <div>
-      <h1>My E-commerce Store</h1>
-      <p>Hello! This is my first React project with Vite!</p>
-    </div>
-  );
+ const [cart, setCart] = useState([]);
+ cosnt [wishlist, setWishlist] = useState([]);
+ const [searchQuery, setSearchQuery] = useState('');
+ const [selectedCategory, setSelectedCategory] = useState('All');
+
+ return(
+  <div>
+    <h1>My E-commerce Store</h1>
+    <p>Cart has {cart.length} items</p>
+    <p>Wishlist has {wishlist.lenght} items</p>
+    <p>You searched for: {searchQuery}</p>
+    <p>Current category: {selectedCategory}</p>
+  </div>
+ )
 }
 
 export default App;
