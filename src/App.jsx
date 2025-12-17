991a7950ff1899d6a4431c448dd45e029a97e7a5
Yashas-webdev <yashasaryashu@gmail.com>
2025-12-18 00:18:47 +0530

    my first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+import { ShoppingCart, Heart, Search, Star } from 'lucide-react';
+
+function App() {
+  
+  return (
+    <div>
+      <h1>My E-commerce Store</h1>
+      <p>Hello! This is my first React project with Vite!</p>
+    </div>
+  );
+}
+
+export default App;
