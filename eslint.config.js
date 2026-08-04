11613cde4fd49a8926e40834b7b7e90ae5753518
Yashas-webdev <yashasaryashu@gmail.com>
2026-08-05 00:40:31 +0530

    made the configuration in eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -5,23 +5,51 @@ import reactRefresh from 'eslint-plugin-react-refresh'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  globalIgnores(['dist', 'node_modules']),
+
+  // Frontend - React / Browser
   {
-    files: ['**/*.{js,jsx}'],
+    files: ['src/**/*.{js,jsx}'],
+
     extends: [
       js.configs.recommended,
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
     ],
+
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+
       parserOptions: {
         ecmaVersion: 'latest',
         ecmaFeatures: { jsx: true },
         sourceType: 'module',
       },
     },
+
+    rules: {
+      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+    },
+  },
+
+  // Backend - Node.js
+  {
+    files: ['server.js', 'backend/**/*.js'],
+
+    extends: [
+      js.configs.recommended,
+    ],
+
+    languageOptions: {
+      ecmaVersion: 'latest',
+      globals: globals.node,
+
+      parserOptions: {
+        sourceType: 'module',
+      },
+    },
+
     rules: {
       'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
     },
