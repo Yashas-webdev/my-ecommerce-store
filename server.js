a53388d957723889886c159538a2a0801e40122e
Yashas-webdev <yashasaryashu@gmail.com>
2026-08-05 00:39:18 +0530

    created the server

---
+++ server.js
@@ -0,0 +1,33 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import cors from 'cors'
+// import connectDB  from './backend/config/db'
+
+//Load env variables
+dotenv.config()
+
+//Connect to MongoDB
+// connectDB()
+
+const app = express()
+
+const PORT = process.env.PORT || 5000;
+
+//Middleware
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
+//Test Route
+app.get('/',(req,res)=>{
+    res.send({
+        message: '🚀 Elegant Shop API is running',
+        status: 'success'
+    })
+})
+
+//Start Server
+
+app.listen(PORT,()=>{
+    console.log(`Server running on port ${PORT}`)
+})
